c647859ce052cd69c70e9676d786693e5c69ffc2
colinjfarmer <cfarme@uw.edu>
2021-04-08 20:05:21 -0700

    added lightyear function

--- inc02b.R
+++ inc02b.R
@@ -4,3 +4,15 @@ lightHour <- lightMinute*60
 lightDay <- lightHour*24
 lightYear <- lightDay*365.2422
 cat("Length of the light year:", lightYear, "/n")
+bhLength <- lightYear * 55
+
+
+lightyear <- function(ly) {
+  c <- 299792458
+  lightMinute <- c*60
+  lightHour <- lightMinute*60
+  lightDay <- lightHour*24
+  lightYear <- lightDay*365.2422
+  x <- lightYear*ly
+  return (x)
+}
